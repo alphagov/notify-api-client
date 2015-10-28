5dc0ed6e6e474ea5b08fd8c8eff104462d01d56a
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-10-28 14:19:35 +0000

    Add setup and versions to api client

--- client/__init__.py
+++ client/__init__.py
@@ -2,3 +2,6 @@ from .errors import APIError, HTTPError, InvalidResponse
 from .errors import REQUEST_ERROR_STATUS_CODE, REQUEST_ERROR_MESSAGE
 
 from .data import DataAPIClient
+
+
+__version__ = '0.0.1'
---
+++ setup.py
@@ -0,0 +1,33 @@
+"""
+Common utils for Digital Marketplace apps.
+"""
+import re
+import ast
+import pip.download
+from pip.req import parse_requirements
+from setuptools import setup, find_packages
+
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('client/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+requirements = list(parse_requirements('requirements.txt',
+                                       session=pip.download.PipSession()))
+
+install_requires = [str(r.req) for r in requirements]
+
+setup(
+    name='notify-api-client',
+    version=version,
+    url='https://github.com/alphagov/notify-api-client',
+    license='MIT',
+    author='GDS Developers',
+    description='API Client for notify API.',
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires
+)
